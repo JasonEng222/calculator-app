50b6eb650b80baadcbf335782713f74482076f7e
Jeng <jason.eng222@gmail.com>
2024-03-27 23:31:51 -0400

    Started cleaning up code

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,65 @@ import React from 'react';
 import { useState } from 'react';
 import './App.css';
 
+const lineOne = [
+  [1],
+  [2],
+  [3],
+  ["x"]
+]
+const lineTwo = [
+  [4],
+  [5],
+  [6],
+  ["-"]
+]
+const lineThree = [
+  [7],
+  [8],
+  [9],
+  ["/"]
+]
+const lineFour = [
+  [0],
+  ["."],
+  ["+"]
+]
+    
 function App() {
-  const [count, setCount] = React.useState(0);
+  const [total, setTotal] = React.useState(0);
   const [number, setNumber] = React.useState(0);
   const [equation, setEquation] = React.useState(0);
-  return (
-    <div className="App">
-      <header className="App-header">
-        {number} 
-      {/* <button onClick={()=> setCount(count equation+number)}>=</button> */}
-      <div className='App-Row'>
-        <button onClick={()=> setNumber(1)}>1</button>
-        <button onClick={()=> setNumber(2)}>2</button>
-        <button onClick={()=> setNumber(3)}>3</button>
-      </div>
-      <div className='App-Row'>
-        <button onClick={()=> setNumber(4)}>4</button>
-        <button onClick={()=> setNumber(5)}>5</button>
-        <button onClick={()=> setNumber(6)}>6</button>
-      </div>
-      <div className='App-Row'>
-        <button onClick={()=> setNumber(7)}>7</button>
-        <button onClick={()=> setNumber(8)}>8</button>
-        <button onClick={()=> setNumber(9)}>9</button>
-      </div>
-      </header>
-    </div>
+    return (
+    <>
+      <input type="text"></input>{number}
+      <table className='calculator'>
+        <tbody>
+          <tr>
+            <td><input type="button" value="C" onClick={()=> setNumber(0)}></input></td>
+          </tr>
+          <tr>
+            {lineOne.map(value => {
+              return (<td> <input onClick={()=> setNumber(value)} type="button" value={value}></input></td>)
+            })}
+          </tr>
+          <tr>
+          {lineTwo.map(value => {
+              return (<td> <input onClick={()=> setNumber(value)} type="button" value={value}></input></td>)
+            })}
+          </tr>
+          <tr>{lineThree.map(value => {
+              return (<td> <input onClick={()=> setNumber(value)} type="button" value={value}></input></td>)
+            })}
+          </tr>
+          <tr>
+          {lineFour.map(value => {
+              return (<td> <input onClick={()=> setNumber(value)} type="button" value={value}></input></td>)
+            })}
+            
+          </tr>
+          </tbody>
+        </table>
+    </>
   );
 }
 
